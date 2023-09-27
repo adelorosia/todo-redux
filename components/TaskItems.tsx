b5299567cf5c0c0d2a268b9129dc6f7b4b53099f
Adel Namazi <adel_orosia@yahoo.com>
2023-09-27 12:54:03 +0200

    task items design fetrig

--- components/TaskItems.tsx
+++ components/TaskItems.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const TaskItems = () => {
   return (
-    <div>TaskItems</div>
+    <ul className='w-full flex flex-col gap-4 bg-green-300 py-8 px-4 rounded-lg'>
+        <li className='bg-white w-full rounded-lg p-2'></li>
+    </ul>
   )
 }
 
